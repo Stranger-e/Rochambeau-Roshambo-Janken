a6ad3fc8a4f96a0a12f5d21d91f02acbc1d19755
Ezekiel OLoo <olooezekiel885@gmail.com>
2023-11-26 12:03:50 +0300

    add the function to let the comp choose randomly

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 //script
 
-console.log("Hello, World!");
+function getComputerChoice (choice1, choice2, choice3) {
+    let choice = Math.floor(Math.random() * 3)
+        switch (choice) {
+            case 0:
+                return choice1;
+            break;
+
+            case 1:
+                return choice2;
+            break;
+
+            case 2:
+                return choice3;
+            break;
+        }
+
+}
+
+let compSelection = getComputerChoice("Rock", "Paper", "Sciccors");
+console.log(compSelection);
+
+let playerSelection = prompt("choose your weapon")
+function playRound (playerSelection, compSelection){
+    if (playerSelection == "Rock" && compSelection == "Paper"){
+        console.log("You lose! Paper beats Rock")
+    }
+    else{
+        console.log("They were not same")
+    }
+}
