b8ed0ad77b0d7cf678edef3f27b4c3b969e3fabd
Ezekiel OLoo <olooezekiel885@gmail.com>
2023-11-27 10:14:00 +0300

    add game function, and keep score

--- script.js
+++ script.js
@@ -18,15 +18,43 @@ function getComputerChoice (choice1, choice2, choice3) {
 
 }
 
-let compSelection = getComputerChoice("Rock", "Paper", "Sciccors");
+let compSelection = getComputerChoice("rock", "paper", "sciccors");
 console.log(compSelection);
 
-let playerSelection = prompt("choose your weapon")
+let getPlayerSelection = prompt("choose your weapon")
+let playerSelection = getPlayerSelection.toLowerCase();
+console.log(playerSelection)
+
 function playRound (playerSelection, compSelection){
-    if (playerSelection == "Rock" && compSelection == "Paper"){
-        console.log("You lose! Paper beats Rock")
+    if (playerSelection == "rock" && compSelection == "paper"){
+        return("You lose! Paper beats Rock")
     }
-    else{
-        console.log("They were not same")
+    else if (playerSelection === "paper" && compSelection === "scissors"){
+        return ("You lose! scissors beats paper")
+    }
+    else if (playerSelection === "scissors" && compSelection=== "rock"){
+        return ("You lose! rock beats sciccors")
+    }
+    else if (playerSelection === compSelection){
+        return ("It was a draw ")
+    }
+    else {
+        return ("You won!")
     }
 }
+
+let result = playRound(playerSelection, compSelection);
+
+let score = 0 
+
+function game() {
+    for(i = 0; i < 5; i++);
+}
+if (result == "You won!") {
+    score++;
+}
+
+
+console.log(score)
+console.log(result)
+
